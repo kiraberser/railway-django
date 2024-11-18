476cb54774edf859e5495cd11c33118a02ff8f37
kira <edwinvega3106@gmail.com>
2024-11-18 04:34:26 -0600

    Static & Procfile Settings

--- backend/settings.py
+++ backend/settings.py
@@ -17,8 +17,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -140,11 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Esto ya parece correcto.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "theme", "static"),
-    os.path.join(BASE_DIR, "theme", "static_src")
+    os.path.join(BASE_DIR, 'theme', 'static'),
+    os.path.join(BASE_DIR, 'theme', 'static_src'),
 ]
 
 # Default primary key field type
