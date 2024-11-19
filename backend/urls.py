dfadf4c015c8c61fa8e05a37ebe309431f5f60df
kira <edwinvega3106@gmail.com>
2024-11-18 18:11:04 -0600

    Prueba

--- backend/urls.py
+++ backend/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
     path('', include('blog.urls')),
     path('', include('home.urls'))
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
